5da5f21ad9f3b8bcbd9bd56df97e17c3610b5d39
wulala <goodbuyer58@gmail.com>
2015-08-16 23:01:14 -0400

    Test commit

--- Algorithm/src/firstPrac/testmain.java
+++ Algorithm/src/firstPrac/testmain.java
@@ -4,13 +4,6 @@ class testmain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/**
-		 * int nums[] = {3,2,6};
-		 * int target = 9;
-		Solution sol = new Solution();
-		int result[] = sol.twoSum(nums, target);
-		System.out.println(result[0]+" and "+result[1]);
-		 */
 		
 		int nums1[] = {1};
 		int nums2[] = {4};
