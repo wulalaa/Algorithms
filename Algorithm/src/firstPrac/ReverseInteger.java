883a3355f3bd4df6a26cd1be20e32c84cb0d84f2
wulala <goodbuyer58@gmail.com>
2015-08-17 01:30:23 -0400

    reverse integer

--- Algorithm/src/firstPrac/ReverseInteger.java
+++ Algorithm/src/firstPrac/ReverseInteger.java
@@ -4,11 +4,22 @@ public class ReverseInteger {
 	public static void main(String [ ] args)
 	{
 		// overflow
-		int res = reverse(4);
+		int res = reverse(-412);
 		System.out.println(res);
 	}
 	
     public static int reverse(int x) {
-        return x;
+        long revx = 0;
+        int sign = x>0? 1:-1;
+        x /= sign;
+        while(x != 0)
+        {
+        	revx = 10*revx + x%10;
+        	x /= 10;
+        }
+        revx *= sign;
+        if (revx > Integer.MAX_VALUE || revx < Integer.MIN_VALUE)
+        	return 0;
+    	return (int) revx;
     }
 }
