762ad625620e44de5b34c3a1394f5bae0575b912
Fengjin Home <cliu121@ufl.edu>
2015-11-11 00:43:59 -0500

    combination sum ii changes

--- Algorithm/src/firstPrac/combinationSumII.java
+++ Algorithm/src/firstPrac/combinationSumII.java
@@ -4,38 +4,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class combinationSumII {
 	public static void main(String[] args) {
-		int[] candidates = {3,4, 5,7,8};
-		List<List<Integer>> sol = combinationSum2(candidates, 7);
+		int[] candidates = {1,1};
+		List<List<Integer>> sol = combinationSum2(candidates, 2);
 		System.out.println(sol);
 	}
 	
 	public static List<List<Integer>> combinationSum2(int[] candidates, int target){
 		List<List<Integer>> sol = new ArrayList<List<Integer>> ();
 		int len = candidates.length;
-		//System.out.println(len+"here");
 		if (len<1)
 			return sol;
 		Arrays.sort(candidates);
 		List<Integer> templist = new ArrayList<Integer> ();
-		System.out.println(templist+"here");
+		//System.out.println(templist+"here");
 		helper(candidates, target, templist, sol,0);
 		return sol;
 	}
 	
 	public static void helper(int[] candidates, int target, List<Integer> templist, List<List<Integer>> sol, int index){
-		if (index==candidates.length || target<0)
+		if (target<0)
 			return;
 		if (target ==0)
 		{
-			sol.add(templist);
-			System.out.println(" add to sol"+sol);
+			List<Integer> x = new ArrayList(templist);
+			sol.add(x);
+		System.out.println(" add to sol"+sol);
 			return;
 		}
 		for (int i=index; i<candidates.length;i++){
-			if (index>0 && candidates[i] == candidates[i-1])
+			if (i>index && candidates[i] == candidates[i-1])
 				continue; // unique solution
+			System.out.println(" i = "+i);
 			templist.add(candidates[i]);
-			System.out.println(templist+"; i = "+i+"; target = "+target);
+			//System.out.println(templist+"; i = "+i+"; target = "+target);
 			helper(candidates, target-candidates[i], templist, sol, i+1);
 			templist.remove(templist.size()-1);
 		}
